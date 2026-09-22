af060a96e4b5577399ba3a63b8baf0094f8d9724
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2026-09-22 04:50:41 +0100

    Add initial Terraform configuration files for VPC setup and AWS provider

---
+++ main.tf
@@ -0,0 +1,128 @@
+# VPC and Internet Gateway
+
+resource "aws_vpc" "tsacademy" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "ts_igw" {
+  vpc_id = aws_vpc.tsacademy.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+# Subnets(2 AZs, 3 tiers = 6 subnets)
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.tsacademy.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-${count.index}"
+  }
+}
+
+resource "aws_subnet" "app" {
+  count             = 2
+  vpc_id            = aws_vpc.tsacademy.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 10)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.project_name}-app-${count.index}"
+  }
+}
+
+
+resource "aws_subnet" "db" {
+  count             = 2
+  vpc_id            = aws_vpc.tsacademy.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 20)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.project_name}-db-${count.index}"
+  }
+}
+
+# NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+
+}
+
+resource "aws_nat_gateway" "tsacademy" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.ts_igw]
+}
+
+
+# Route Tables
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.tsacademy.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ts_igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.tsacademy.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.tsacademy.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "app" {
+  count          = 2
+  subnet_id      = aws_subnet.app[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "db" {
+  count          = 2
+  subnet_id      = aws_subnet.db[count.index].id
+  route_table_id = aws_route_table.private.id
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+
+}
+
+variable "vpc_cidr" {
+  type = string
+}
