31d0aed33e0d6d1ad526194499ec3d0fcd456c86
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2026-09-24 17:49:00 +0100

    Add S3 backend configuration for Terraform state management

--- providers.tf
+++ providers.tf
@@ -11,3 +11,13 @@ provider "aws" {
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "default"
 }
+
+terraform {
+  backend "s3" {
+    bucket       = "tf-class-state-mbas"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
